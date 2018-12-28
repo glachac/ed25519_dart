a61fd54962cd5ac5c69e216dd3fc2fe3ebe2ca34
Göran Krampe <goran.krampe@gmail.com>
2018-12-28 05:12:18 +0100

    Moving to BigInt

--- lib/src/ed25519_dart_base.dart
+++ lib/src/ed25519_dart_base.dart
@@ -12,21 +12,23 @@ import 'dart:typed_data' show Uint8List;
 import 'package:pointycastle/pointycastle.dart' show Digest;
 
 // Magic constants
-const int baseX =
-15112221349535400772501151409588531511454012693041857206046113283949847762202;
-const int baseY =
-46316835694926478169428394003475163141307993866256225615783033603165251855960;
+final baseX = BigInt.parse(
+    "15112221349535400772501151409588531511454012693041857206046113283949847762202");
+final baseY = BigInt.parse(
+    "46316835694926478169428394003475163141307993866256225615783033603165251855960");
 const int bits = 256;
-const int d =
-37095705934669439343138083508754565189542113879843219016388785533085940283555;
-const int I =
-19681161376707505956807079304988542015446066515923890162744021073123829784752;
-const int primeL =
-7237005577332262213973186563042994240857116359379907606001950938285454250989;
-const int primeQ =
-57896044618658097711785492504343953926634992332820282019728792003956564819949;
+final d = BigInt.parse(
+    "37095705934669439343138083508754565189542113879843219016388785533085940283555");
+final I = BigInt.parse(
+    "19681161376707505956807079304988542015446066515923890162744021073123829784752");
+final primeL = BigInt.parse(
+    "7237005577332262213973186563042994240857116359379907606001950938285454250989");
+final primeQ = BigInt.parse(
+    "57896044618658097711785492504343953926634992332820282019728792003956564819949");
 
-List<int> basePoint = const [baseX % primeQ, baseY % primeQ];
+List<BigInt> basePoint = [baseX % primeQ, baseY % primeQ];
+
+final ONE = BigInt.from(1);
 
 /// Clamps the lower and upper bits as required by the specification.
 /// Returns [bytes] with clamped bits.
@@ -53,26 +55,25 @@ Uint8List bytesFromList(List<int> lst) => new Uint8List.fromList(lst);
 ///
 ///     var l = new List<int>.generate(32, (int i) => i + i); // [0, ..., 60, 62]
 ///     bytesToInteger(l); // 28149809252802682310...81719888435032634998129152
-int bytesToInteger(List<int> bytes) {
-  num value = 0;
+BigInt bytesToInteger(List<int> bytes) {
+  var value = BigInt.zero;
   bytes = bytes.sublist(0, 32);
   for (var i = 0; i < bytes.length; i++) {
-    value += bytes[i] * pow(bits, i);
+    value += BigInt.from(bytes[i] * pow(bits, i));
   }
   ;
-  return value.toInt();
+  return value;
 }
 
 /// Converts integer [intVal] into [x, y] point.
 ///
 ///     decodePoint(28149809252802682310); // [2063...9514, 28149809252802682310]
-List<int> decodePoint(int intVal) {
-  var y = intVal % pow(2, (bits - 1)).toInt();
+List<BigInt> decodePoint(BigInt intVal) {
+  var y = intVal % (BigInt.from(pow(2, (bits - 1))));
   var x = xRecover(y);
-  if ((x & 1) != ((intVal >> (bits - 1))) & 1) {
+  if ((x & ONE) != ((intVal >> (bits - 1))) & ONE) {
     x = primeQ - x;
   }
-  ;
   return [x, y];
 }
 
@@ -80,24 +81,24 @@ List<int> decodePoint(int intVal) {
 /// Returns sum of points.
 ///
 ///     edwards([1,2], [1,2]); // [38630...2017, 20917...5802]
-List<int> edwards(List<int> P, List<int> Q) {
-  int x1, y1, x2, y2, x3, y3;
+List<BigInt> edwards(List<BigInt> P, List<BigInt> Q) {
+  BigInt x1, y1, x2, y2, x3, y3;
   x1 = P[0];
   y1 = P[1];
   x2 = Q[0];
   y2 = Q[1];
-  x3 = (x1 * y2 + x2 * y1) * modularInverse(1 + d * x1 * x2 * y1 * y2);
-  y3 = (y1 * y2 + x1 * x2) * modularInverse(1 - d * x1 * x2 * y1 * y2);
+  x3 = (x1 * y2 + x2 * y1) * modularInverse(ONE + d * x1 * x2 * y1 * y2);
+  y3 = (y1 * y2 + x1 * x2) * modularInverse(ONE - d * x1 * x2 * y1 * y2);
   return [x3 % primeQ, y3 % primeQ];
 }
 
 /// Encodes point [P] into [Uint8List].
 ///
 ///     encodePoint([1,2]); // [2, 0, ..., 0, 0, 128]
-Uint8List encodePoint(List<int> P) {
+Uint8List encodePoint(List<BigInt> P) {
   var x = P[0];
   var y = P[1];
-  var encoded = integerToBytes(y + ((x & 1) << 255), 32);
+  var encoded = integerToBytes(y + ((x & ONE) << 255), 32);
   return encoded;
 }
 
@@ -110,36 +111,43 @@ Uint8List Hash(Uint8List m) => new Digest('SHA-512').process(m);
 /// Converts integer [e] into [Uint8List] with length [length].
 ///
 ///     integerToBytes(1, 32); // [0, 4, ... 0, 0, 0, 0, 0]
-Uint8List integerToBytes(int e, int length) {
+Uint8List integerToBytes(BigInt e, int length) {
   var byteList = new Uint8List(length);
   for (var i = 0; i < length; i++) {
-    byteList[0 + i] = (e >> (i * 8));
+    byteList[0 + i] = (e >> (i * 8)) as int;
   }
-  ;
   return byteList;
 }
 
 /// Returns [bool] that that indicates if point [P] is on curve.
 ///
 ///     isOnCurve([1, 2]); // false
-bool isOnCurve(List<int> P) {
+/*bool isOnOldCurve(List<int> P) {
   int x, y;
   x = P[0];
   y = P[1];
   var onCurve = (-x * x + y * y - 1 - d * x * x * y * y) % primeQ == 0;
   return onCurve;
+}*/
+
+bool isOnCurve(List<BigInt> P) {
+  BigInt x, y;
+  x = P[0];
+  y = P[1];
+  var val = (-x * x + y * y - ONE - d * x * x * y * y) % primeQ;
+  return val == BigInt.zero;
 }
 
 /// Returns the modular multiplicative inverse of integer [z]
 /// and modulo [primeQ].
 ///
 ///     modularInverse(2); // 28948022...41009864396001978282409975
-int modularInverse(int z) => z.modInverse(primeQ);
+BigInt modularInverse(BigInt z) => z.modInverse(primeQ);
 
 /// Returns integer [x] to the power of `pow(2, p)` with modulo [primeQ].
 ///
 ///     modularPow(3, 2); // 81
-int modularPow(int x, int p) => x.modPow(pow(2, p).toInt(), primeQ);
+int modularPow(int x, int p) => x.modPow(pow(2, p).toInt(), primeQ as int);
 
 /// Generates public key from given secret key [sk].
 /// Public key is [Uint8List] with size 32.
@@ -155,13 +163,13 @@ Uint8List publicKey(Uint8List sk) {
 /// Returns result of scalar multiplication of point [P] by integer [e].
 ///
 ///     scalarMult([1,2], 10); // [298...422, 480...666]
-List<int> scalarMult(List<int> P, int e) {
-  if (e == 0) {
-    return [0, 1];
+List<BigInt> scalarMult(List<BigInt> P, BigInt e) {
+  if (e == BigInt.zero) {
+    return [BigInt.zero, ONE];
   }
-  var Q = scalarMult(P, e ~/ 2);
+  var Q = scalarMult(P, e ~/ BigInt.from(2));
   Q = edwards(Q, Q);
-  if (e & 1 > 0) {
+  if (e & ONE > BigInt.zero) {
     Q = edwards(Q, P);
   }
   ;
@@ -179,15 +187,13 @@ Uint8List secretKey([int seed]) {
   Random randGen;
   if (seed == null) {
     randGen = new Random.secure();
-  }
-  else {
+  } else {
     randGen = new Random(seed);
   }
   var randList = new List<int>.generate(1024, (_) => randGen.nextInt(bits));
   var clamped = bitClamp(Hash(bytesFromList(randList)));
   return clamped;
 }
-
 
 /// Creates signature for message [message] by using secret key [secretKey]
 /// and public key [pubKey].
@@ -209,8 +215,7 @@ Uint8List sign(Uint8List message, Uint8List secretKey, Uint8List pubKey) {
   var digest = bytesToInteger(Hash(bytesFromList(preDigest)));
   var signature = new List<int>.from(scalar);
   signature.addAll(integerToBytes(
-      (msgSecretAsInt + digest * bytesToInteger(bitClamp(secretHash))).toInt() %
-          primeL,
+      (msgSecretAsInt + digest * bytesToInteger(bitClamp(secretHash))) % primeL,
       32));
   return bytesFromList(signature);
 }
@@ -239,7 +244,7 @@ bool verifySignature(Uint8List signature, Uint8List message, Uint8List pubKey) {
   var signatureScalar = scalarMult(basePoint, signatureInt);
   var pubKeyScalar = scalarMult(decodePoint(bytesToInteger(pubKey)), hashInt);
   var edwardsPubKeyScalar =
-  edwards(decodePoint(bytesToInteger(sigSublist)), pubKeyScalar);
+      edwards(decodePoint(bytesToInteger(sigSublist)), pubKeyScalar);
   var verified = (signatureScalar.length == edwardsPubKeyScalar.length &&
       signatureScalar[0] == edwardsPubKeyScalar[0] &&
       signatureScalar[1] == edwardsPubKeyScalar[1]);
@@ -250,15 +255,15 @@ bool verifySignature(Uint8List signature, Uint8List message, Uint8List pubKey) {
 /// Returns recovered `x`.
 ///
 ///     xRecover(10); // 246881771...00105170855113893569705867530
-int xRecover(int y) {
-  var xx = (y * y - 1) * modularInverse(d * y * y + 1);
+BigInt xRecover(BigInt y) {
+  var xx = (y * y - ONE) * modularInverse(d * y * y + ONE);
 
-  var x = xx.modPow((primeQ + 3) ~/ 8, primeQ);
-  if ((x * x - xx) % primeQ != 0) {
+  var x = xx.modPow((primeQ + BigInt.from(3)) ~/ BigInt.from(8), primeQ);
+  if ((x * x - xx) % primeQ != BigInt.zero) {
     x = (x * I) % primeQ;
   }
 
-  if (x % 2 != 0) {
+  if (x % BigInt.from(2) != BigInt.zero) {
     x = primeQ - x;
   }
 
